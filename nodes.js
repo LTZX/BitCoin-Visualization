cfa06b1c1f4c9baaeec07f4804535e647f6207d7
Zhiyi Xu <yzxu@ucdavis.edu>
2018-11-29 17:53:35 -0800

    nodes

--- nodes.js
+++ nodes.js
@@ -1,72 +1,83 @@
+function drag(simulation) {
+    function dragstarted(d) {
+        if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+    }
 
-  const links = data.links.map(d => Object.create(d));
-  const nodes = data.nodes.map(d => Object.create(d));
-  const simulation = forceSimulation(nodes, links).on("tick", ticked);
+    function dragged(d) {
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
+    }
 
-  const svg = d3.select(DOM.svg(width, height))
-      .attr("viewBox", [-width / 2, -height / 2, width, height]);
+    function dragended(d) {
+        if (!d3.event.active) simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
+    }
 
-  const link = svg.append("g")
-      .attr("stroke", "#999")
-      .attr("stroke-opacity", 0.6)
-    .selectAll("line")
-    .data(links)
-    .enter().append("line")
-      .attr("stroke-width", d => Math.sqrt(d.value));
+    return d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended);
+}
 
-  const node = svg.append("g")
-      .attr("stroke", "#fff")
-      .attr("stroke-width", 1.5)
-    .selectAll("circle")
-    .data(nodes)
-    .enter().append("circle")
-      .attr("r", 5)
-      .attr("fill", color)
-      .call(drag(simulation));
+function forceSimulation(nodes, links) {
+    return d3.forceSimulation(nodes)
+        .force("link", d3.forceLink(links).id(d => d.id))
+        .force("charge", d3.forceManyBody())
+        .force("x", d3.forceX())
+        .force("y", d3.forceY());
+}
 
-  node.append("title")
-      .text(d => d.id);
+d3.json("data.json", function(error, data) {
+    if (error) throw error;
 
-  function ticked() {
-    link
-        .attr("x1", d => d.source.x)
-        .attr("y1", d => d.source.y)
-        .attr("x2", d => d.target.x)
-        .attr("y2", d => d.target.y);
+    const links = data.links//.map(d => Object.create(d));
+    const nodes = data.nodes//.map(d => Object.create(d));
+    const simulation = forceSimulation(nodes, links).on("tick", ticked);
+    var color = d3.scaleOrdinal(d3.schemeCategory20);
 
-    node
-        .attr("cx", d => d.x)
-        .attr("cy", d => d.y);
-  }
+    var nodewith = document.getElementById('network').offsetWidth;
+    var nodeheight = document.getElementById('network').offsetHeight;
 
+    var network = d3.select("#network")
+        .append("svg")
+        .attr("width", nodewith)
+        .attr("height", nodeheight)
 
-  d3.forceSimulation(nodes)
-      .force("link", d3.forceLink(links).id(d => d.id))
-      .force("charge", d3.forceManyBody())
-      .force("x", d3.forceX())
-      .force("y", d3.forceY());
+    const link = network.append("g")
+        .attr("stroke", "#999")
+        .attr("stroke-opacity", 0.6)
+      .selectAll("line")
+      .data(links)
+      .enter().append("line")
+        .attr("stroke-width", d => Math.sqrt(d.value))
+        .attr("transform", "translate("+ (nodewith/2) + "," + (nodeheight/2) +")");
 
-  const scale = d3.scaleOrdinal(d3.schemeCategory10);
-  return d => scale(d.group);
+    const node = network.append("g")
+        .attr("stroke", "#fff")
+        .attr("stroke-width", 1.5)
+      .selectAll("circle")
+      .data(nodes)
+      .enter().append("circle")
+        .attr("r", 5)
+        .attr("fill", function(d){ return color(d.group); })
+        .attr("transform", "translate("+ (nodewith/2) + "," + (nodeheight/2) +")")
+        .call(drag(simulation));
 
-  function dragstarted(d) {
-    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-    d.fx = d.x;
-    d.fy = d.y;
-  }
+    node.append("title")
+        .text(d => d.id);
 
-  function dragged(d) {
-    d.fx = d3.event.x;
-    d.fy = d3.event.y;
-  }
+    function ticked() {
+      link
+          .attr("x1", d => d.source.x)
+          .attr("y1", d => d.source.y)
+          .attr("x2", d => d.target.x)
+          .attr("y2", d => d.target.y);
 
-  function dragended(d) {
-    if (!d3.event.active) simulation.alphaTarget(0);
-    d.fx = null;
-    d.fy = null;
-  }
-
-  d3.drag()
-      .on("start", dragstarted)
-      .on("drag", dragged)
-      .on("end", dragended);
+      node
+          .attr("cx", d => d.x)
+          .attr("cy", d => d.y);
+    }
+})
